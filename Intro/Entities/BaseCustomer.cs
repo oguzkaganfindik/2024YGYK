5c74129200cf6f3294988e816023782afbc47d32
Oguzkagan Findik <oguzfindik14@gmail.com>
2024-02-16 03:34:24 +0300

    11.02.2024

---
+++ Intro/Entities/BaseCustomer.cs
@@ -0,0 +1,6 @@
+﻿namespace Intro.Entities;
+public class BaseCustomer
+{
+    public int Id { get; set; }
+    public string CustomerNumber { get; set; }
+}
---
+++ Intro/Entities/CorporateCustomer.cs
@@ -0,0 +1,6 @@
+﻿namespace Intro.Entities;
+public class CorporateCustomer : BaseCustomer
+{
+    public string Name { get; set; }
+    public string TaxNumber{ get; set; }
+}
